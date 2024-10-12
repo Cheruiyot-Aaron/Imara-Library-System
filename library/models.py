270d73ad897eb7f63168398e21effb9b380d131f
cheruiyot-Aaron <acheruiyot090@gmail.com>
2024-10-12 18:26:46 +0300

    Student models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,4 @@
+   
 from django.db import models
 
 class Book(models.Model):
@@ -14,3 +15,17 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=50, unique=True)
+    full_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    mobile_number = models.CharField(max_length=15)
+    grade = models.CharField(max_length=50)
+    current_loans = models.ManyToManyField(Book, blank=True)
+    fines = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.full_name
+
